067953fda54621c6526e4c962871e394c019e0f0
Bekir Kiper <bekirkiper58@gmail.com>
2023-02-22 22:01:05 +0100

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+
+
+# create namespace (test)
+kubectl apply -f namespace.yaml
+
+# create ingress controller
+minikube -p my-nodes addons enable ingress
+
+# create secret
+kubectl apply -f secret.yaml
+
+# create deployment 
+kubectl apply -f deployment-mysql.yaml
+kubectl apply -f deployment-wordpress.yaml
+kubectl apply -f deployment-pma.yaml
+
+
--- start.sh
+++ start.sh
@@ -4,18 +4,3 @@
 minikube start --nodes 3 -p my-nodes
 minikube dashboard -p my-nodes
 
-# create namespace (test)
-kubectl apply -f namespace.yaml
-
-# create ingress controller
-minikube -p my-nodes addons enable ingress
-
-# create secret
-kubectl apply -f secret.yaml
-
-# create deployment 
-kubectl apply -f deployment-mysql.yaml
-kubectl apply -f deployment-wordpress.yaml
-kubectl apply -f deployment-pma.yaml
-
-
